ba1faa009ff0fb93f29e7489ccc8f6d6f915f61f
Afsanay <97282348+Afsanay@users.noreply.github.com>
2023-06-15 18:26:54 +0000

    co2

--- chatvision-web/src/pages/index.js
+++ chatvision-web/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import { useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { createParser } from "eventsource-parser";
 
 
 export default function Home() {
@@ -14,29 +15,73 @@ const SYSTEM_MESSAGE = "You are ChatVision, an Artificial Intelligence ChatBot c
 const [userMessage,setUserMessage] = useState("");
 const [messages,setMessages] = useState([{"role":"system","content":SYSTEM_MESSAGE}]);
 
-async function sendRequest() {
+const sendRequest = async () => {
+  const updatedMessages = [
+    ...messages,
+    {
+      role: "user",
+      content: userMessage,
+    },
+  ];
 
-  const newmessage = {role:"user",content:userMessage};
-  const newMessages = [...messages,newmessage];
-  setMessages(newMessages);
+  setMessages(updatedMessages);
   setUserMessage("");
 
-  const response = await fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer pk-jdLDAvPgJzeEupRRYbtCKEPnVzqjyXvDBrjAqVfTXDnkRrst`,
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: newMessages,
-    }),
-  });
-  const responseJson = await response.json();
-  const newbotMessage = responseJson.choices[0].message;
-  const newMess = [...newMessages,newbotMessage];
-  setMessages(newMess);
-}
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer pk-jdLDAvPgJzeEupRRYbtCKEPnVzqjyXvDBrjAqVfTXDnkRrst`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: updatedMessages,
+        stream: true,
+      }),
+    });
+
+    const reader = response.body.getReader();
+
+    let newMessage = "";
+    const parser = createParser((event) => {
+      if (event.type === "event") {
+        const data = event.data;
+        if (data === "[DONE]") {
+          return;
+        }
+        const json = JSON.parse(event.data);
+        const content = json.choices[0].delta.content;
+
+        if (!content) {
+          return;
+        }
+
+        newMessage += content;
+
+        const updatedMessages2 = [
+          ...updatedMessages,
+          { role: "assistant", content: newMessage },
+        ];
+
+        setMessages(updatedMessages2);
+      } else {
+        return "";
+      }
+    });
+
+    // eslint-disable-next-line
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      const text = new TextDecoder().decode(value);
+      parser.feed(text);
+    }
+  } catch (error) {
+    console.error("error");
+    window.alert("Error:" + error.message);
+  }
+};
 
 
   return (
